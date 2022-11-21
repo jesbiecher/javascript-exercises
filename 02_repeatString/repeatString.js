961e7dd0a2cbb028eb7004259c6955a46d7b4449
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-11-21 17:49:25 +0100

    2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (myWord, times) {
+    if (times < 0) return 'ERROR';
+    let word = '';
+    for (let iteration = 0; iteration < times; iteration++) {
+        word += myWord;
+    }
+    return word;
 };
 
 // Do not edit below this line
