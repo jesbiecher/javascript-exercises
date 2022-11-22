83aea7f06c08dbfdd62265f861ce4c92d7990ff8
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-11-22 16:38:38 +0100

    7th  exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  var converted = (temp - 32) / 1.8;
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var converted = (temp * 1.8) + 32;
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
@@ -9,3 +13,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//Fahrenheit to Celsius : (°F − 32) ÷ 1.8 = °C
+//Celsius to Fahrenheit : (°C × 1.8) + 32 = °F
